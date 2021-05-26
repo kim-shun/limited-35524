dd8a5175b39cc6f1e93839307e86ab839d585614
kim-shun <kimura1417080@gmail.com>
2021-05-26 15:58:32 +0900

    messagesの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'messages/index'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
@@ -8,5 +9,7 @@ Rails.application.routes.draw do
   end
   root to: "home#index"
   resources :users, only: [:edit, :update, :show]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages only: [:index, :new, :create]
+  end
 end
