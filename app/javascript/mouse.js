79684303cdb00b525237fb88b27ab1de3fd5dc83
kim-shun <kimura1417080@gmail.com>
2021-05-28 17:58:06 +0900

    少しjavascript

---
+++ app/javascript/mouse.js
@@ -0,0 +1,14 @@
+function mouse() {
+  const pullDownButton = document.getElementById("chat-room-btn")
+
+  pullDownButton.addEventListener('mouseover', function(){
+    this.setAttribute("style", "background-color:#FFBEDA;")
+  })
+
+  pullDownButton.addEventListener('mouseout', function(){
+    this.removeAttribute("style", "background-color:#FFBEDA;")
+  })
+
+};
+
+window.addEventListener('load', mouse)
