7fc5afde6866b92bd4a42babc4d90730f610e7a8
kim-shun <kimura1417080@gmail.com>
2021-06-08 12:50:37 +0900

    ogiriのテストコード

--- spec/factories/ogiris.rb
+++ spec/factories/ogiris.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :ogiri do
-    
+    first_text_id { Faker::Number.between(from: 2, to: 16) }
+    second_text_id { Faker::Number.between(from: 2, to: 17) }
+    text {Faker::Lorem.sentence}
+    association :user
   end
 end
--- spec/models/ogiri_spec.rb
+++ spec/models/ogiri_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Ogiri, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @ogiri = FactoryBot.build(:ogiri)
+    end
+
+    context '内容に問題ない場合' do
+      it 'テキストが全て埋まっていれば登録できること' do
+        expect(@ogiri).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'first_text_idが1では登録できないこと' do
+        @ogiri.first_text_id = 1
+        @ogiri.valid?
+        expect(@ogiri.errors.full_messages).to include("文頭のテキストを選択してください。")
+      end
+
+      it 'second_text_idが1では登録できないこと' do
+        @ogiri.second_text_id = 1
+        @ogiri.valid?
+        expect(@ogiri.errors.full_messages).to include("末尾のテキストを選択してください。")
+      end
+
+      it '自由回答欄が空では登録できないこと' do
+        @ogiri.text = ""
+        @ogiri.valid?
+        expect(@ogiri.errors.full_messages).to include("自由回答欄を入力してください")
+      end
+    end
+  end
 end
