9b63e54cc239c9eeb44df2866ed4c0da61df01fb
kim-shun <kimura1417080@gmail.com>
2021-06-15 10:41:56 +0900

    未ログインでも回答が見られる

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :search_user, only: [:index, :search]
 
   def index
